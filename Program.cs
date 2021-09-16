0bfb0bdb24e85199401929b9556bf508b7c81f11
nathan <ennemmjones@gmail.com>
2021-09-16 19:24:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamGitPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
